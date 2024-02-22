9e0be59cb307a6949405fad691945a4b9ec0c177
dxia2p <d.xia111@gmail.com>
2024-02-22 09:56:53 -0700

    finished it

--- main.py
+++ main.py
@@ -45,27 +45,48 @@ def getMenuSelection():
 def displayAll(baby_data):
     # Display All Baby Data
     print("\nDISPLAY ALL")
-
+    for i in baby_data: # loop through all the data and print it out
+        print(f"{i['name']} (Rank: {i['rank']}, Gender: {i['gender']})")
 
 def searchGender(baby_data):
     # Dislay All Baby Names based on Gender
     print("\nSEARCH BY GENDER")
+    gender = input("Enter a gender (Boy/Girl): ")
+    for i in baby_data:
+        if i['gender'] == gender: # only print the data if the gender matches input gender
+            print(f"{i['name']} (Rank: {i['rank']}, Gender: {i['gender']})")
 
 
 def searchRank(baby_data):
     # Display All Baby Names based on Rank
     print("\nSEARCH BY RANK")
-
+    minRank = int(input("Enter a minimum rank: ")) - 1 # minus one because array starts at 0
+    maxRank = int(input("Enter a maximum rank: "))
+    for i in range(minRank * 2, maxRank * 2): # because there are 2 names for each rank, just multiply the minimum and maximum rank by 2
+        baby = baby_data[i]
+        print(f"{baby['name']} (Rank: {baby['rank']}, Gender: {baby['gender']})") # print the data
+    print(f"Number of names found: {maxRank * 2 - minRank * 2}") 
 
 def searchStartLetter(baby_data):
     # Insert User Item into a Position
     print("\nSEARCH BY START LETTER")
-
+    startingLetter = input("Enter a starting letter: ")
+    namesFound = 0
+    for i in baby_data:
+        if i['name'][0] == startingLetter: # get the first character, if it is equal to the inputted starting letter then print the data
+            namesFound += 1
+            print(f"{i['name']} (Rank: {i['rank']}, Gender: {i['gender']})")
+    print(f"Number of names found: {namesFound}")
 
 def searchNameLength(baby_data):
     # Remove item from position
     print("\nSEARCH BY NAME LENGTH")
-
-
+    nameLength = int(input("Enter a name length: "))
+    namesFound = 0
+    for i in baby_data:
+        if len(i['name']) == nameLength: # if the length of the name matches input, then print the data
+            namesFound += 1
+            print(f"{i['name']} (Rank: {i['rank']}, Gender: {i['gender']})")
+    print(f"Number of names found: {namesFound}")
 # Invoke main to begin program
 main()
